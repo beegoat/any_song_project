3331c0d47b61fecefd777f96c60768808b75a5d4
Sandy Lee <goatjswn@naver.com>
2023-01-27 04:24:02 +0900

    database uri changed
    from local server to AWS server

--- back/index.js
+++ back/index.js
@@ -11,7 +11,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const port = 3001
 const pool = mysql.createPool({
-    host: "localhost",
+    host: "43.201.140.172:3306",
     user: "deepal",
     password: "deepal",
     database: "anysong",
